db3b0cbf784dfbf7442e118080356f122430e020
Sam Snow <boombea10@gmail.com>
2024-12-17 19:10:33 +0330

    changing base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  // app: {
-  //   baseURL: '/university_projectNuxt/'
-  // },
+  app: {
+    baseURL: '/university_projectNuxt/'
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
 
